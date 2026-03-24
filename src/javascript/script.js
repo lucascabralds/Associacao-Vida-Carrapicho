543b93e0da3af94a77058a78760b88065ddd22f2
Kevin <kevin.sovetim@gmail.com>
2026-03-24 13:46:46 -0300

    Kevin mexeu aqui

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
-    }); 
+
+        let icone = $('#mobile_btn').find('img');
+
+        if ($('#mobile_menu').hasClass('active')) {
+
+            icone.attr('src', './img/x-solid-full.svg');
+        } else {
+
+            icone.attr('src', './img/bars-solid-full.svg');
+        }
+
+    });
 });
