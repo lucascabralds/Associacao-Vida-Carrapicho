1202db9c69cc59dcc0ffce407831973811e19faa
Caiogomes4567 <caiogomes4567@gmail.com>
2026-05-19 19:28:18 -0300

    Update index.js

--- Projeto-carrapicho/api/index.js
+++ Projeto-carrapicho/api/index.js
@@ -25,7 +25,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
     fileFilter: (req, file, cb) => {
         const allowedTypes = /jpeg|jpg|png|gif|webp/;
         const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
